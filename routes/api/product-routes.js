e5bc2960c21e59cac77c62b801d9532ad5339964
amadeamargo <amadeamargo@gmail.com>
2022-03-18 05:34:51 -0500

    added get routes for pdoucts

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,25 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [Category, 
+    {model: Tag,
+    through: ProductTag}
+  ],
+  }) .then ((productData) => {
+    res.json(productData)
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findbyPk((req.params.id), 
+  {include: [Category, Tag ]},
+  ).then((productData) => {
+    res.json(productData)
+  })
 });
 
 // create new product
