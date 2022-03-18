7445b1ae47f8e28979af8c20761a9e00e47d326a
amadeamargo <amadeamargo@gmail.com>
2022-03-18 05:54:47 -0500

    added put and delete routes for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -38,12 +38,39 @@ router.post('/', (req, res) => {
   })
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async(req, res) => {
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      }
+    });
+    if(!categoryData) {
+      res.status(404).json({message: `No category found for this id`})
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status (500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy(req.body, {
+      where: {
+        id: req.params.id,
+      }
+    });
+    if(!categoryData) {
+      res.status(404).json({message: `No category found for this id`})
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status (500).json(err);
+  }
 });
 
 module.exports = router;
